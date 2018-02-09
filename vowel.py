86ed9b22a36aa02d9e1fb4f404526c6317c24236
Ramyaveerasekar <36103153+Ramyaveerasekar@users.noreply.github.com>
2018-02-09 21:38:34 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module5
+# Purpose:
+#
+# Author:      VINO
+#
+# Created:     09/02/2018
+# Copyright:   (c) VINO 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+chr=input("enter any character")
+if(chr=='a' or chr=='e' or chr=='i' or
+ chr=='o' or chr=='u' '):
+    print( "is a vowel")
+else:
+	print("is a consonant")
